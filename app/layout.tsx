575cf12e898e3d01ac7dd1b16c409f7358700cdc
chimfwembeC <chimfwembek@kamstar.tech>
2024-05-18 01:57:04 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 // import Head from 'next/head';
-import { Sidebar } from 'primereact/sidebar';
-import { Button } from 'primereact/button';
-import { ScrollTop } from 'primereact/scrolltop';
+import { Sidebar } from "primereact/sidebar";
+import { Button } from "primereact/button";
+import { ScrollTop } from "primereact/scrolltop";
+import { Metadata } from "next";
 // import "primereact/resources/primereact.min.css";
 // import "primeicons/primeicons.css";
 // import "./globals.css";
@@ -14,10 +14,10 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
-const metadata: Metadata = {
+export const metadata: Metadata = {
   title: "Chimfwembe's Portfolio",
-  description: "Showcasing my development and personal skills. Welcome to my portfolio.",
-  icon: "/favicon.ico"
+  description:
+    "Showcasing my development and personal skills. Welcome to my portfolio.",
 };
 
 const RootLayout: React.FC<LayoutProps> = ({ children }) => {
@@ -25,25 +25,19 @@ const RootLayout: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <html>
-         <div>
-      {/* <Head> */}
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href={metadata.icon} />
-      {/* </Head> */}
-     
-    
-      
+      <div>
         <body>
-        <header className="flex justify-between p-4 bg-gray-200">
-        <Button icon="pi pi-bars"  />
-        header
-      </header>
+          <header className="flex justify-between p-4 bg-gray-200">
+            <Button icon="pi pi-bars" />
+            header
+          </header>
           <main>{children}</main>
           <ScrollTop />
-          <footer className="bg-gray-200 p-4 text-center">Footer content</footer>
-        </body>        
-    </div>
+          <footer className="bg-gray-200 p-4 text-center">
+            Footer content
+          </footer>
+        </body>
+      </div>
     </html>
   );
 };
